4053454b9c01288adae0e916d58c6d316bcbbd9a
Gaurangi Puradkar <gaurangifb@gmail.com>
2023-07-08 01:51:50 +0530

    slider first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import {useEffect, useState} from "react";
+import NetflixCard from "./components/NetflixCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./App.css"
+
+const App = () => {
+  const [myData, setMyData] = useState([]);
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "7526a2abc4mshcaf8928adaea272p10cdfajsna4c956f56250",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    try {
+      //const res = await fetch("https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=e", options);
+      const data = await res.json();
+      setMyData(data.titles);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <>
+      <Carousel responsive={responsive} showDots={true}>
+        {/* {myData.map((currItem) => {
+          return (
+            <NetflixCard key={currItem.summary.id} actualData={currItem} />
+          );
+        })} */}
+      </Carousel>
+    </>
+  );
+};
+
+export default App;
